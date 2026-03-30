2c23f1ccab1c6844a9424f786d7e24e53da660c7
German <fedosihingerman@yandex.ru>
2026-03-30 16:05:17 +0300

    simple btns change

--- src/components/BoardLists.jsx
+++ src/components/BoardLists.jsx
@@ -16,28 +16,32 @@ export const BoardLists = () => {
         <div className="board">
             <div className="board__controls">
                 <button className="board__btn-add"
-                    onClick={()=>addListWithTimer(2)}>
-                    Добавить список на 2 секунды
+                    onClick={() => addListWithTimer(2)}>
+                    2 секунды
                 </button>
                 <button className="board__btn-add"
-                    onClick={()=>addListWithTimer(60*60*1)}>
+                    onClick={() => addListWithTimer(60 * 60 * 1)}>
                     1 час
                 </button>
                 <button className="board__btn-add"
-                    onClick={()=>addListWithTimer(60*60*2)}>
+                    onClick={() => addListWithTimer(60 * 60 * 2)}>
                     2 часа
                 </button>
                 <button className="board__btn-add"
-                    onClick={()=>addListWithTimer(60*60*24)}>
+                    onClick={() => addListWithTimer(60 * 60 * 24)}>
                     1 день
                 </button>
                 <button className="board__btn-add"
-                    onClick={()=>addListWithTimer(60*60*24*2)}>
+                    onClick={() => addListWithTimer(60 * 60 * 24 * 2)}>
                     2 дня
                 </button>
                 <button className="board__btn-add"
-                    onClick={()=>addListWithTimer(60*60*24*7)}>
-                    на неделю
+                    onClick={() => addListWithTimer(60 * 60 * 24 * 7)}>
+                    1 неделя
+                </button>
+                <button className="board__btn-add"
+                    onClick={() => addListWithTimer(60 * 60 * 24 * 14)}>
+                    2 недели
                 </button>
             </div>
             <div className="board__lists">
